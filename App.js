fe5b4f0a1a6e33ab2d4e45ea98f61830acb22c90
hassane31 <hassane31@kali.SAMSUNG>
2025-10-28 23:53:38 +0100

    first how to create react FW

---
+++ App.js
@@ -0,0 +1,10 @@
+function MyBtn (){
+    return <button>im a button</button>;
+}
+
+export default function MyApp (){
+    return (<div>
+        <h1>Welcome to my Web</h1>
+        <MyBtn />
+    </div>);
+}
